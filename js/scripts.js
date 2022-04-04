b2f7349cb7b34051a91181f87cc2e693393f76ce
Marcus <marcanthonyzpadilla@gmail.com>
2022-04-04 12:03:20 -0700

    update toggle and info
    Co-authored-by: Brady Diamond <BradyDiamond@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,84 @@
-// Business Logic for AddressBook ---------
-function toD() {
-  this.contacts = {};
+// Business Logic for Places ---------
+function Places() {
+  this.place = {};
   this.currentId = 0;
 }
 
-AddressBook.prototype.addContact = function(contact) {
-  contact.id = this.assignId();
-  this.contacts[contact.id] = contact;
+Places.prototype.addPlace = function(place) {
+  beenPlaces.id = this.assignId();
+  this.places[place.id] = place;
 };
 
-AddressBook.prototype.assignId = function() {
+Places.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 };
 
-AddressBook.prototype.findContact = function(id) {
-  if (this.contacts[id] != undefined) {
-    return this.contacts[id];
+Places.prototype.findPlace = function(id) {
+  if (this.places[id] != undefined) {
+    return this.places[id];
   }
   return false;
 };
 
-AddressBook.prototype.deleteContact = function(id) {
-  if (this.contacts[id] === undefined) {
+Places.prototype.deletePlace = function(id) {
+  if (this.places[id] === undefined) {
     return false;
   }
-  delete this.contacts[id];
+  delete this.places[id];
   return true;
 };
 
-// Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
+// Business Logic for Specific Place---------
+let paris = new Place('Paris', 'France', '1942');
+let rome = new Place('Rome', 'Italy', '1922');
+let london = new Place('London', 'UK', '2004');
+
+function Place(city, country, timeOfYear) {
+  this.city = city;
+  this.country= country;
+  this.timeOfYear= timeOfYear;
 }
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+Place.prototype.wholePlace = function() {
+  return this.city + " " + this.country + " " + this.timeOfYear;
 };
+
+
+//User Interface Logic
+
+$(document).ready(function(){
+
+    //UI selectors
+  const parisSelect = $("#one");
+  const romeSelect = $("#two");
+  const londonSelect = $("#three");
+
+  //UI expressions
+  let cityAndCountryAndDate = paris.wholePlace();
+  let romeVacation = rome.wholePlace();
+  let londonVacation = london.wholePlace();
+
+  parisSelect.click(function(event) {
+    event.preventDefault();
+    //click to show
+    parisSelect.text(cityAndCountyAndDate);
+    romeSelect.hide();
+    $("#three").hide();
+  });
+  romeSelect.click(function(event) {
+    event.preventDefault();
+    //click to show
+    romeSelect.text(romeVacation);
+    $("#one").hide();
+    $("#three").hide();
+  });
+  londonSelect.click(function(event) {
+    event.preventDefault();
+    //click to show
+    londonSelect.text(londonVacation);
+    $("#one").hide();
+    $("#two").hide();
+  });
+
+});
